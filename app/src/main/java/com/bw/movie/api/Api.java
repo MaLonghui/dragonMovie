c8e5daedc56e7b70cd1c4cade1d892a81629d946
牛浩 <1971658757@qq.com>
2019-03-17 15:59:11 +0800

    no message

--- app/src/main/java/com/bw/movie/api/Api.java
+++ app/src/main/java/com/bw/movie/api/Api.java
@@ -1,24 +1,13 @@
 package com.bw.movie.api;
 
 public class Api {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     //base
     public static final String BASE_URL = "http://mobile.bwstudent.com/";
+    //注册
+    public static final String REGIST_URL = "movieApi / user / v1 / registerUser";
+    //登录
+    public static final String LOGIN_URL = "movieApi / user / v1 / login";
+
     //查询热门电影列表
     public static final String FILM_RE_URL = "movieApi/movie/v1/findHotMovieList";
     //查询正在上映电影列表
--- app/src/main/java/com/bw/movie/api/ApiServer.java
+++ app/src/main/java/com/bw/movie/api/ApiServer.java
@@ -6,37 +6,29 @@ import com.bw.movie.bean.ReFilmBean;
 import com.bw.movie.bean.ShangFilmBean;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import io.reactivex.Observable;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.HeaderMap;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
 import retrofit2.http.Url;
 
 public interface ApiServer {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //注册
+    @POST
+    @FormUrlEncoded
+    Observable<RegistBean> regist(@Url String url, @FieldMap Map<String,Object> parms);
+    //登录
+    @POST
+    @FormUrlEncoded
+    Observable<LoginBean> login(@Url String url, @Field("phone")String phone, @Field("pwd")String pwd);
 
 
     //热门电影
