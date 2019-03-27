cef7835c924b33ec00e8901f5c5db8e6b1b5b8bd
牛浩 <1971658757@qq.com>
2019-03-27 20:33:13 +0800

    牛浩

--- app/src/main/java/com/bw/movie/fragment/alreadymoney/AlreadyMoneyContract.java
+++ app/src/main/java/com/bw/movie/fragment/alreadymoney/AlreadyMoneyContract.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import com.bw.movie.mvp.BasePresenter;
 import com.bw.movie.mvp.BaseView;
 
+import java.util.Map;
+
 /**
  * MVPPlugin
  *  邮箱 784787081@qq.com
@@ -12,10 +14,10 @@ import com.bw.movie.mvp.BaseView;
 
 public class AlreadyMoneyContract {
     interface View extends BaseView {
-        
+        void AlreadyTicketView(Object obj);
     }
 
     interface  Presenter extends BasePresenter<View> {
-        
+        void AlreadyTicketPresenter(Map<String,Object> headMap, Map<String,Object> parms);
     }
 }
